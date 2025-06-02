fcfc4b293884899139f4d2fad64f0daa833624de
MinaWilson92 <mina.a.wilson@gmail.com>
2025-06-02 19:46:34 +0300

    Create testapp.js

---
+++ testapp.js
@@ -0,0 +1,10 @@
+// App.js
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px', backgroundColor: 'red', color: 'white' }}>
+      TEST: React is working!
+    </div>
+  );
+}
